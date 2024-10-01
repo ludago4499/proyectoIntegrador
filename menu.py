ab8ae72732b54cba0f8af50bb0d806db03f2138c
ludago <luisgonzaleza.lol@gmail.com>
2024-10-01 12:03:49 -0600

    Nuevo archivo Menu

---
+++ menu.py
@@ -0,0 +1,43 @@
+def print_menu () -> None:
+    print("Ingrese el objeto que se vendió: ")
+    print("Blizzard (1)")
+    print ("Cono (2)")
+    print ("Pastel (43")
+    print("Banana Split (4)")
+    print ("Extras (5)")
+
+
+def blizzard():
+
+    nieveSize = [300,500,800] # cantidad de nieve a usar
+    # precio = [60,70,80]
+    # costo = [30,35,40]
+    print("Cuál es el tamaño? ")
+    print("Chico (1)")
+    print("Mediano (2)")
+    print("Grande (3)") 
+
+
+
+def cono():
+    precio = []
+    print("Cuál es el tamaño? ")
+    print("Cono sencillo (1)")
+    print("Cono doble (2)")
+    print("Cono triple (3)") 
+
+
+def pastel():
+    print ("Qué tipo de pastel?")
+    print( "Pastel Oreo (1)")
+    print ("Pastel Choco Xtreme (2)")
+    print( "Pastel de Fresa (3)")
+
+
+def banana_split():
+    platanos = 1
+    fresa = 3
+    cereza = 3
+    crema_batida = 0.5
+    nieve = 500
+
