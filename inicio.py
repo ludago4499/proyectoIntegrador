7659a6259cab4006ebd8d6b7a0189c59a2d10067
ludago <luisgonzaleza.lol@gmail.com>
2024-10-02 15:20:59 -0600

    Inicio updates

--- inicio.py
+++ inicio.py
@@ -12,12 +12,12 @@ def main():
         seleccion = input("Seleccion: ")
         try:
             seleccion = int(seleccion)  
-            flag = False  
+            flag = False  # se sale del while
         except ValueError:
             print("Hubo un error en su selección. Verifique nuevamente.")  # mensaje de error
-        time.sleep(1) # Se espera un segundo    
+        time.sleep(1) # Se espera un segundo y se vuelve a repetir el while  
     valor = menuPrincipal.seleccion_menu_principal(seleccion) # checa si hay un error (-1)
-    if (valor == -1):
+    if (valor == - 1):
         main()
 
 main()
